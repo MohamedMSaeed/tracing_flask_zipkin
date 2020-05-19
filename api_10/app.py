fcaf447da754eb7b8fc4d4cbe289a5e0d26e6d3d
ahmed.itman <ahmed.itman@swvl.com>
2020-05-20 01:08:33 +0200

    Configure api_10 with mysql

--- api_10/app.py
+++ api_10/app.py
@@ -4,9 +4,13 @@ from py_zipkin.zipkin import zipkin_span, create_http_headers_for_new_span, Zipk
 from py_zipkin.request_helpers import create_http_headers
 from py_zipkin.encoding import Encoding
 import sys, os
-
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'http://mysql_10'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'mysql_10'
+app.config['MYSQL_DB'] = 'mysql_10'
 
 
 def default_handler(encoded_span):
@@ -31,9 +35,20 @@ def log_request_info():
     app.logger.debug('Body: %s', request.get_data())
 
 @zipkin_client_span(service_name='api_10', span_name='just_message_api10')
-def just_message_08():
+def just_message():
     app.logger.info("Just message from API 10")
     return 'OK'
+
+@zipkin_client_span(service_name='api_10', span_name='connect_mysql')
+def connect_mysql():
+    app.logger.info("Connect to mysql")
+    mysql = MySQL(app)
+    firstName = "swvl"
+    lastName = "swvl"
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO MyUsers(firstName, lastName) VALUES (%s, %s)", (firstName, lastName))
+    mysql.connection.commit()
+    cur.close()
 
 @app.route('/')
 def index():
@@ -52,8 +67,8 @@ def index():
         sample_rate=100,
         encoding=Encoding.V2_JSON
     ):
-        just_message_08()
-        call_api_09()
+        just_message()
+        connect_mysql()
 
     return 'OK', 200
 
